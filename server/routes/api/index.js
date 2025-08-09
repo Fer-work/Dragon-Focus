306575212b58e2be7cedd795a3bbf836cfd72515
Fernando Sanchez Gutierrez <fer.work117@gmail.com>
2025-08-08 19:21:04 -0600

    Update index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -8,8 +8,8 @@ import categoryRoutes from "./categories.js";
 
 const router = Router();
 
-// router.use("/users", userRoutes);
-// router.use("/tasks", taskRoutes);
+router.use("/users", userRoutes);
+router.use("/tasks", taskRoutes);
 // router.use("/sessions", sessionRoutes);
 router.use("/categories", categoryRoutes);
 
